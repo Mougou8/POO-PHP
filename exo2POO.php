a6c34cc311e4bf7e9747dbadf685c320a7904700
Mougou8 <riquet.mougoundan@gmail.com>
2025-02-21 10:05:29 +0100

    exo3 POO PHP fini

--- exo2POO.php
+++ exo2POO.php
@@ -11,17 +11,17 @@ class Voiture {
     }
 
     public function getInfos() {
-        return "Marque : $this->_marque, Modèle : $this->_modele";
+        return "Marque : $this->marque, Modèle : $this->modele";
     }
 }
 // classe de dérive VoitureElec= héritage
 class VoitureElect extends Voiture {
-    private $autonomie;
+    private $_autonomie;
 
     public function __construct($marque, $modele, $autonomie){
         //Ajout du parent= pour éviter la répétition de code
         parent::__construct($marque, $modele);
-        $this->autonomie = $autonomie;
+        $this->_autonomie = $autonomie;
 }
     public function getInfos(){
       return parent::getInfos() . ", Autonomie : $this->_autonomie km";
@@ -30,12 +30,14 @@ class VoitureElect extends Voiture {
 // Instanciation des objets
 $v1 = new Voiture("Peugeot","408");
 $ve2 = new voitureElect("BMW", "i3", 100);
+$v3 = new Voiture("Citroên","C4");
+$ve4 = new voitureElect("Toyota", "i3", 150);
 
 // Affichage des informations
 echo $v1->getInfos() ."<br>";
 echo $ve2->getInfos() ."<br>";
-
-
+echo $v3->getInfos() ."<br>";
+echo $ve4->getInfos() ."<br>";
 // Héritage: définit des rélations hiérarchiques entre les classes ,
 // de sorte que les attributs et méthodes communs peuvent être réutilisés.
 //En définissant des attributs et des comportements de base dane classe principale
--- exo3POO.php
+++ exo3POO.php
@@ -8,32 +8,32 @@ private  $_dateNaissance;
 public function __construct($nom, $prenom, $dateNaissance) {
     $this->_nom = $nom;
     $this->_prenom = $prenom;
+    // le constructeur initialise la date de naissance  en utilisant la classe DateTime
     $this->_dateNaissance = new DateTime($dateNaissance);
 }
 
-//Méthode pour calculer l'âge
-public function getAge(){ //
+// Méthode getAge: calcule l'âge en utilisant la méthode diff de la classe DateTime
+public function getAge(){  
     $aujourdhui = new DateTime();
-    $age = $aujourdhui->diff($this->dateNaissance);
-    return $age->y;// retourne l'âge en années
+    $age = $aujourdhui->diff($this->_dateNaissance);
+    return $age->y; // l' attribut y =>retourne l'âge en années
 }
-// Méthode pour afficherles informations
+// Méthode pour afficher les informations
 public function afficherInfos() {
-    echo"{$this->prenom} {$this->nom} a " . $this-> getAge() . " ans.<br>";
+    echo"{$this->_prenom} {$this->_nom} a " . $this-> getAge() . " ans.<br>"; 
     }
 }
 // Instanciation des objets
 $sp1 = new Personne ("DUPONT", "Michel", "1980-02-19");
 $sp2 = new Personne ("DUCHEMIN", "Alice", "1985-01-17");
+$sp3 = new Personne ("DUPOND", "Gérard", "1983-03-18");
+$sp4 = new personne ("MULLER", "Mathilde", "1987-04-20");
 
 // Affichage
 $sp1->afficherInfos();
 $sp2->afficherInfos();
+$sp3 ->afficherInfos();
+$sp4 ->afficherInfos();
 
  
-
-// // public function afficherInfos() {
-// //     echo "Nom: " . $this->nom . "\n";
-// //     echo "Prénom" . $this->prenom . "\n";
-// //     echo "DateNaissance" . $this->
 
